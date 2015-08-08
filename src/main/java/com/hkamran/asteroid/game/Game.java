6a9c801826fb47773e7ae73db5e57f481a99216b
hkamran <hkamran490@gmail..com>
2015-08-08 12:59:41 -0400

    Game.java - fixed synch issues with bullets and asteroids

--- src/main/java/com/hkamran/asteroid/game/Game.java
+++ src/main/java/com/hkamran/asteroid/game/Game.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.swing.JComponent;
 
@@ -25,19 +27,21 @@ public class Game extends JComponent {
 	private static final long serialVersionUID = 1L;
 
 	public Dimension screenSize;
-	
+
 	private Player player;
-	private ArrayList<Asteriod> asteroids = new ArrayList<Asteriod>();
-	private ArrayList<Bullet> bullets = new ArrayList<Bullet>();
+	private List<Asteriod> asteroids = Collections.synchronizedList(new ArrayList<Asteriod>());
+	private List<Bullet> bullets = Collections.synchronizedList(new ArrayList<Bullet>());
 	private Integer score = 0;
 	private static Integer highscore = 0;
-	
+
 	public Boolean isGameOver = false;
 	public KeyboardListener keyboard;
 
 	/**
 	 * Constructor.
-	 * @param screenSize screensize of the window.
+	 * 
+	 * @param screenSize
+	 *            screensize of the window.
 	 */
 	public Game(Dimension screenSize) {
 		this.screenSize = screenSize;
@@ -46,8 +50,7 @@ public class Game extends JComponent {
 
 		// Add Player
 		keyboard = new KeyboardListener();
-		player = new Player(new Point(screenSize.getWidth() / 2.0,
-				screenSize.getHeight() / 2.0), keyboard);
+		player = new Player(new Point(screenSize.getWidth() / 2.0, screenSize.getHeight() / 2.0), keyboard);
 		this.addKeyListener(keyboard);
 
 		asteroids.add(new Asteriod(new Point(50.0, 50.0), 10.0));
@@ -61,40 +64,41 @@ public class Game extends JComponent {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
-		//Draw black background.
+		// Draw black background.
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
 		g.setColor(Color.BLACK);
 		g.fillRect(1, 1, (int) Math.floor(this.getWidth()) - 2, (int) Math.floor(this.getHeight()) - 2);
 
 		player.draw(g);
 
-		//TODO: Stop concurrent modification
+		// TODO: Stop concurrent modification
 
-		//Draw bullets.
-		for (GameObject bullet : bullets) {
-			bullet.draw(g);
+		// Draw bullets.
+		synchronized (bullets) {
+			for (GameObject bullet : bullets) {
+				bullet.draw(g);
+			}
 		}
-		
-		//Draw asteroids.
-		for (GameObject asteriod : asteroids) {
-			asteriod.draw(g);
+		// Draw asteroids.
+		synchronized (asteroids) {
+			for (GameObject asteriod : asteroids) {
+				asteriod.draw(g);
+			}
 		}
-		
-		//Draw Scores.
+		// Draw Scores.
 		g.setColor(Color.WHITE);
 		g.drawString("HighScore: " + highscore, this.getWidth() - 100, 30);
 		g.drawString("Score: " + score, 20, 30);
-		
-		
+
 		if (isGameOver) {
-			//Draw Game over sign.
+			// Draw Game over sign.
 			g.setColor(Color.WHITE);
-			g.setFont(new Font("Arial", Font.PLAIN, 24)); 
-			g.drawString("GAME OVER", this.getWidth()/2 - 80, this.getHeight()/2);
-			
+			g.setFont(new Font("Arial", Font.PLAIN, 24));
+			g.drawString("GAME OVER", this.getWidth() / 2 - 80, this.getHeight() / 2);
+
 			g.setColor(Color.WHITE);
-			g.setFont(new Font("Arial", Font.PLAIN, 12)); 
-			g.drawString("(Press Space to reset)", this.getWidth()/2 - 70, this.getHeight()/2 + 20);
+			g.setFont(new Font("Arial", Font.PLAIN, 12));
+			g.drawString("(Press Space to reset)", this.getWidth() / 2 - 70, this.getHeight() / 2 + 20);
 		}
 	}
 
@@ -102,24 +106,24 @@ public class Game extends JComponent {
 	 * Actions calculated during a game tick.
 	 */
 	public void tick() {
-		
+
 		if (!isGameOver) {
 			player.tick();
 		}
 		if (score > highscore) {
 			highscore = score;
 		}
-		
+
 		ArrayList<Bullet> newBullets = player.bullets;
 		for (Bullet bullet : newBullets) {
 			bullets.add(bullet);
 		}
 		player.bullets = new ArrayList<Bullet>();
-		
+
 		ArrayList<Bullet> oldBullets = new ArrayList<>();
 		ArrayList<Asteriod> destroyedAsteroid = new ArrayList<>();
 
-		//Manage bullets.
+		// Manage bullets.
 		for (Bullet bullet : bullets) {
 			for (Asteriod asteroid : asteroids) {
 				Boolean result = bullet.isColliding(asteroid);
@@ -133,15 +137,15 @@ public class Game extends JComponent {
 				oldBullets.add(bullet);
 			}
 		}
-       
-		//Remove any old bullets
+
+		// Remove any old bullets
 		for (Bullet bullet : oldBullets) {
 			bullets.remove(bullet);
 		}
 
 		ArrayList<Asteriod> newAsteroids = new ArrayList<>();
-		
-		//Remove destroyed asteroids and add new smaller asteroids.
+
+		// Remove destroyed asteroids and add new smaller asteroids.
 		for (Asteriod asteroid : destroyedAsteroid) {
 			score += 25;
 			asteroid.dies();
@@ -149,8 +153,8 @@ public class Game extends JComponent {
 			asteroids.remove(asteroid);
 		}
 		asteroids.addAll(newAsteroids);
-		
-		//Manage asteroids.
+
+		// Manage asteroids.
 		for (GameObject asteriod : asteroids) {
 			asteriod.tick();
 			if (asteriod.isColliding(player)) {
