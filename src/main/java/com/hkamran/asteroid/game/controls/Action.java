d04db9dad5c64044b04789456cbe8b0a3c57a50c
hkamran490 <hkamran490@gmail.com>
2017-12-27 22:19:09 -0500

    added different keys for +/- delays

--- src/main/java/com/hkamran/asteroid/game/controls/Action.java
+++ src/main/java/com/hkamran/asteroid/game/controls/Action.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  * @author hkamran
  */
 public enum Action {
-	left(65,37), right(68,39), up(87,38), down(83,40), space(32), enter(10), increase(107), decrease(109);
+	left(65,37), right(68,39), up(87,38), down(83,40), space(32), enter(10), increase(107, 61), decrease(109, 45);
 	
 	public ArrayList<Integer> keycodes = new ArrayList<>();
 	
